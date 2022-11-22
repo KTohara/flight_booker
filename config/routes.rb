710d6460731281130284162a45ea6719f92fab43
kenta <ktohara@gmail.com>
2022-11-21 23:11:17 -0500

    add nav partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "flights#index"
+  
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
   devise_for :users
 
   resources :flights, only: [:index]
